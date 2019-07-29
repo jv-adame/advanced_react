14400a1c16079f2320ed85ace5d83f98043af2d0
John Vincent Adame <johnvincent.adame@gmail.com>
2019-07-29 16:21:59 -0400

    05 - Intro to Styled Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,12 @@
 import React, {Component} from "react";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+    background: red;    
+    font-size: ${props => (props.huge ? "100px" : "50px")};
+`;
 
 class Page extends Component{
     render(){
