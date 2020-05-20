30dfe75d0f07ce0e16969258d5586ea2ce5b112a
John Vincent Adame <johnvincent.adame@gmail.com>
2020-05-20 19:17:07 -0400

    34: Permissions Management

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,5 +1,5 @@
 const {forwardTo} = require('prisma-binding');
-
+const {hasPermission} = require('../utils');
 const Query = {
     item: forwardTo('db'),
     items: forwardTo('db'),
@@ -14,6 +14,17 @@ const Query = {
             }, 
             info
         );
+    },
+    async users(parent, args, ctx, info){
+        // 1. Check if the user is logged in
+        if(!ctx.request.userId){
+            throw new Error('You must be logged in!');
+        }
+        // 1. Check if the user has the permissions to query all the users
+        console.log(ctx.request.userId);
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+        // 2. if they do, query all the users
+        return ctx.db.query.users({}, info);
     }
 };
 
